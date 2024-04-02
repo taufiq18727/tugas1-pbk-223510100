0ef84e24eb76dd1be57a9052f5f152fe88c646a1
Taufiqurrahman <taufiqurrahman187@student.uir.ac.id>
2024-04-02 13:51:50 +0700

    Resolved conflicts

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      <a href="" target="" rel="noopener"><img src="c:\Users\clean.user\Pictures\gmail.png" alt="gmail"  width="15" height="15">Contact: <br> +6285263855186</a><br>
+      <a href="https://api.whatsapp.com/send/?phone=%2B6285263855186&text&type=phone_number&app_absent=0" target="_blank" rel="noopener"><img src="c:\Users\clean.user\Pictures\wa.png" alt="+62 852 6385 5186" width="18" height="18">085263855186</a> <br>
+      <a href="https://www.instagram.com/taufiq.297?igsh=NDF5cXltbzc2ZHVz" target="_blank" rel="noopener"><img src="c:\Users\clean.user\Pictures\ig.jpeg" alt="Instagram"  width="15" height="15">taufiq.297</a><br>
+      <a href="https://mail.google.com/mail/u/0/#inbox" target="_blank" rel="noopener"><img src="c:\Users\clean.user\Pictures\gmail.png" alt="gmail"  width="15" height="15">qurrahman224@gmail.com</a><br>
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Pendidikan</template>
+SD AL-AZHAR SYIFA BUDI PEKANBARU<br>
+SMP NEGERI 5 PEKANBARU<br>
+SMA NEGERI 9 PEKANBARU<br>
+TEKNIK INFORMATIKA UNIVERSITAS ISLAM RIAU
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Specialist</template>
+    Software Engineer
+    memahami cara-cara pengembangan peranti lunak termasuk pembuatan, pemeliharaan, manajemen organisasi pengembangan peranti lunak dan manajemen mutu.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Hobi</template>
+Olahraga(Futsal, badminton, berenang, jogging)<br>
+Bermain Game<br>
+Nonton<br>
+Mendengar Musik
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Community</template>
+Tergabung dalam komunitas:
+<ul>
+  <li>Teknik Informatika</li>
+  <li>Data Analyst Indonesia</li>
+  <li>UI/UX Designer Indonesia</li>
+</ul>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Status</template>
+
+    Single (jomblo)
+  </WelcomeItem>
+</template>
